c14071300e8025bb2cd6020fcb96de7c29d41591
sunita0987 <sunita0987546@gmail.com>
2025-05-26 09:36:55 +0530

    font color add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,8 @@ import React from "react";
 function Home() {
   return (
     <div>
-      {/* <img src=" src/assets/images/sunitaverma.jpg"></img> */}
       <h1 className="text-5xl font-normal text-center mt-30 text-red-600 ">
-         Developer SUNITA VERMA
+        Developer SUNITA VERMA
       </h1>
       <h2 className="text-4xl font-bold text-center mt-10 text-sky-800">
         Welcome to My profile
@@ -15,18 +14,20 @@ function Home() {
           with a Bachelor's degree in Computer Applications (BCA) from DN
           College, Hisar (2024).
         </span>
-        Currently, I am pursuing a front-end development in <span className="font-bold text-red-700"> HISAR,</span>
-        where I'm learning and building projects using <span className="font-bold text-green-800">HTML, CSS, JavaScript,
-        React, and Tailwind CSS.</span> I enjoy creating clean, responsive, and
-        user-friendly web applications. Some of my recent projects include a
-        calorie counter app, a dice game, and a forum leaderboard that displays
-        real-time data. I believe in writing clean, maintainable code and
-        continuously improving my skills. I'm now seeking opportunities where I
-        can apply my knowledge, grow as a developer, and contribute to impactful
-        projects.
+        Currently, I am pursuing a front-end development in
+        <span className="font-bold text-red-700"> HISAR,</span>
+        where I'm learning and building projects using
+        <span className="font-bold text-green-800">
+          HTML, CSS, JavaScript, React, and Tailwind CSS.
+        </span>
+        I enjoy creating clean, responsive, and user-friendly web applications.
+        Some of my recent projects include a calorie counter app, a dice game,
+        and a forum leaderboard that displays real-time data. I believe in
+        writing clean, maintainable code and continuously improving my skills.
+        I'm now seeking opportunities where I can apply my knowledge, grow as a
+        developer, and contribute to impactful projects.
       </p>
     </div>
-  );
+  )
 }
-
 export default Home;
